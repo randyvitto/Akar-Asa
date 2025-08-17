189b9bdfeff58955867d28d6b4f13424deb9a7c5
randyvitto <randyvitto25@gmail.com>
2025-08-17 15:11:52 +0700

    add video on Collection

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -17,7 +17,7 @@ const Collection = () => {
         </div>
         <div className="intro-image">
            <video width="100%" height="auto" controls autoPlay muted loop>
-              <source src="/watu-maladong.mp4" type="video/mp4" />
+              <source src="/CACAVIDEO.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
         </div>
